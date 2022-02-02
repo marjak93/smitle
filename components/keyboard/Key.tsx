2be6c66cf65f737547dc0a0c54d1ac021307f0c6
Martin Jakobsen <martin.jakobsen@godtlevert.no>
2022-02-02 19:57:04 +0100

    Replace colors

--- components/keyboard/Key.tsx
+++ components/keyboard/Key.tsx
@@ -1,15 +1,15 @@
-import { ReactNode } from 'react'
-import classnames from 'classnames'
-import { KeyValue } from '../../lib/keyboard'
-import { CharStatus } from '../../lib/statuses'
+import { ReactNode } from "react";
+import classnames from "classnames";
+import { KeyValue } from "../../lib/keyboard";
+import { CharStatus } from "../../lib/statuses";
 
 type Props = {
-  children?: ReactNode
-  value: KeyValue
-  width?: number
-  status?: CharStatus
-  onClick: (value: KeyValue) => void
-}
+  children?: ReactNode;
+  value: KeyValue;
+  width?: number;
+  status?: CharStatus;
+  onClick: (value: KeyValue) => void;
+};
 
 export const Key = ({
   children,
@@ -19,30 +19,30 @@ export const Key = ({
   onClick,
 }: Props) => {
   const classes = classnames(
-    'flex items-center justify-center rounded mx-0.5 text-xs font-bold cursor-pointer select-none dark:text-white',
+    "flex items-center justify-center rounded mx-0.5 text-xs font-bold cursor-pointer select-none dark:text-white",
     {
-      'bg-slate-200 dark:bg-slate-600 hover:bg-slate-300 active:bg-slate-400':
+      "bg-slate-200 dark:bg-slate-600 hover:bg-slate-300 active:bg-slate-400":
         !status,
-      'bg-slate-400 text-white': status === 'absent',
-      'bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white':
-        status === 'correct',
-      'bg-orange-500 hover:bg-orange-600 active:bg-orange-700 dark:bg-orange-700 text-white':
-        status === 'present',
+      "bg-slate-400 text-white": status === "absent",
+      "bg-green-500 hover:bg-green-600 active:bg-green-700 text-white":
+        status === "correct",
+      "bg-yellow-500 hover:bg-yellow-600 active:bg-yellow-700 dark:bg-yellow-700 text-white":
+        status === "present",
     }
-  )
+  );
 
   const handleClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
-    onClick(value)
-    event.currentTarget.blur()
-  }
+    onClick(value);
+    event.currentTarget.blur();
+  };
 
   return (
     <button
-      style={{ width: `${width}px`, height: '58px' }}
+      style={{ width: `${width}px`, height: "58px" }}
       className={classes}
       onClick={handleClick}
     >
       {children || value}
     </button>
-  )
-}
+  );
+};
