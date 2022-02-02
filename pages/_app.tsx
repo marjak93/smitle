cf6cc17adf8f3b2a8b6bc217085023d8a1378a01
Martin Jakobsen <martin.jakobsen@godtlevert.no>
2022-02-02 18:31:44 +0100

    copy word game project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import "../index.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  if (typeof window === "undefined") {
+    return null;
+  }
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./{pages,lib,constants,components}/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
